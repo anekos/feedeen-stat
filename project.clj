7d20501e644ccd25f351fcd0dfdf1df386b3e9ea
anekos <anekos@snca.net>
2019-11-06 21:49:08 +0900

    Implement

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
 (defproject feedeen-stat "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :main feedeen-stat.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.10.0"]
+                 [clj-jwt "0.1.1"] ; https://github.com/liquidz/clj-jwt
+                 [clj-time "0.15.2"] ; http://clj-time.github.io/clj-time/doc/index.html
+                 [org.clojure/tools.namespace "0.3.1"] ; https://github.com/clojure/tools.namespace
+                 [org.clojure/data.json "0.2.6"]])
+
+
--- src/feedeen_stat/core.clj
+++ src/feedeen_stat/core.clj
@@ -1,6 +1,69 @@
-(ns feedeen-stat.core)
+(ns feedeen-stat.core
+  (:gen-class)
+  (:require [clj-http.client :as client]
+            [clj-jwt.core  :refer :all]
+            [clj-jwt.key   :refer [private-key public-key]]
+            [clj-time.core :refer [now plus hours minutes]]
+            [clj-time.coerce :refer [to-epoch]]
+            [clojure.data.json :as json]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn feedeen-api-url
+  [path]
+  (str
+    ; "http://localhost:8000/api"
+    "https://www.feedeen.com/api"
+    path))
+
+
+(defn make-claim
+  [dev-client-id]
+  {:iss dev-client-id
+   :aud (feedeen-api-url "/auth")
+   :exp (-> (now) (plus (hours 1)))})
+
+(defn make-jwt-token
+  [claim dev-secret]
+  (-> claim jwt (sign :HS256 dev-secret) to-str))
+
+(defn get-access-token
+  [jwt-token]
+  (->
+    "/auth"
+    feedeen-api-url
+    (client/post
+      {:as :x-www-form-urlencoded
+       :form-params {:grant_type "urn:ietf:params:oauth:grant-type:jwt-bearer"
+                     :assertion jwt-token}})
+    :body
+    (json/read-str :key-fn keyword)
+    :access_token))
+
+(defn api-request
+  [access-token path]
+  (-> path
+      feedeen-api-url
+      (client/get
+        {:headers {:Authorization (str "Bearer " access-token)}})
+      :body
+      (json/read-str :key-fn keyword)))
+
+(defn get-feeds
+  [access-token]
+  (api-request access-token "/api/feeds"))
+
+(defn get-items-count
+  [access-token & {:keys [mode] :or {mode "default"}}]
+  (api-request access-token
+               (str "/items/count?mode=" mode)))
+
+
+(defn -main
+  "main"
+  ([]
+   (println "Usage: $0 <CLIENT_ID> <SECRET>")
+   (System/exit 1))
+  ([dev-client-id dev-secret]
+   (let [access-token (-> (make-claim dev-client-id) (make-jwt-token dev-secret) get-access-token)
+         total (-> (get-items-count access-token :mode "total") :total)]
+     (print total))))
